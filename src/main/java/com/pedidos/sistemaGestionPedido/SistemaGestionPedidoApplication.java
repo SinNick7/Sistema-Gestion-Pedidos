b38d94b6abb96e56480d8b3c0ce045ac40e15f69
SinNick7 <juan.tomairo@unmsm.edu.pe>
2021-12-27 23:54:43 -0500

    feat: Add spring initializr

---
+++ src/main/java/com/pedidos/sistemaGestionPedido/SistemaGestionPedidoApplication.java
@@ -0,0 +1,13 @@
+package com.pedidos.sistemaGestionPedido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaGestionPedidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaGestionPedidoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pedidos/sistemaGestionPedido/SistemaGestionPedidoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedidos.sistemaGestionPedido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaGestionPedidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
